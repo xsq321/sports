13b1c3ee036682599cf4a93d6d12da0d08d6d826
xie <308327267@qq.com>
2022-05-10 11:12:25 +0800

    基本功能完成

--- sports/src/components/Login.vue
+++ sports/src/components/Login.vue
@@ -17,15 +17,33 @@
         </el-form-item>
         <el-form-item class="footer">
           <el-button type="primary" @click="submitForm()">登录</el-button>
-          <el-button @click="registerForm(loginForm)" style="margin-left:50px">注册</el-button>
+          <el-button style="margin-left:50px" @click="registerVisible=true">注册</el-button>
         </el-form-item>
       </el-form>
     </el-card>
+    <!-- 注册对话框 -->
+    <el-dialog title="注册" :visible.sync="registerVisible">
+      <el-form :model='registerForm' :rules="rules" class="demo-ruleForm">
+        <el-form-item label="用户名" :label-width="formLabelWidth" prop="username">
+          <el-input v-model="registerForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="formLabelWidth" prop="password">
+          <el-input v-model="registerForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="姓名" :label-width="formLabelWidth" prop="truename">
+          <el-input v-model="registerForm.truename"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="registerVisible = false">取 消</el-button>
+        <el-button type="primary" @click="register">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { getUser } from '../api/user'
+import { getUser, insertUser } from '../api/user'
 export default {
   data () {
     return {
@@ -42,7 +60,20 @@ export default {
           { required: true, message: '请输入密码', trigger: 'blur' },
           { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
         ],
-      }
+      },
+      registerVisible: false,
+      registerForm: {
+        username: '',
+        password: '',
+        rank: '运动员',
+        truename: '',
+      },
+      rules: {
+        username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+        truename: [{ required: true, message: '请输入真实姓名', trigger: 'blur' }]
+      },
+      formLabelWidth: '120px',
     }
   },
   methods: {
@@ -61,6 +92,20 @@ export default {
           this.$message.error('登录失败，请检查用户名与密码')
         }
       })
+    },
+    register () {
+      if (this.registerForm.username && this.registerForm.password && this.registerForm.truename) {
+        // 注册就相当于向users表中添加用户
+        insertUser(this.registerForm).then(res => {
+          if (res.data.status === 1) {
+            this.$message({
+              message: '注册成功',
+              type: 'success'
+            })
+          }
+        })
+        this.registerVisible = false
+      }
     }
   },
 }
